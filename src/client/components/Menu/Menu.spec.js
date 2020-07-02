13886594c03a02da752171321c255168de1a7eb3
Gal Dubitski <galmail@gmail.com>
2020-07-02 21:05:13 +0100

    unit test for Menu component

---
+++ src/client/components/Menu/Menu.spec.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../setupTests";
+import { shallow, mount } from "enzyme";
+import Menu from "./Menu";
+
+describe("Menu", () => {
+  let wrapper;
+
+  it("onItemRemoved callback will get called when an item is removed ", () => {
+    // NOT IMPLEMENTED YET
+  });
+});
